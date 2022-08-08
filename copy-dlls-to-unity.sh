bd024c2a804a3facb65c7aee071eb818a1522699
Ricky Willis <ricky.willis@spaceapegames.com>
2022-08-08 18:22:21 +0100

    test script change to see if the build permission error is due to running the script or the script contents

--- copy-dlls-to-unity.sh
+++ copy-dlls-to-unity.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp ./JsonFx/JsonFx.Json.UnitTests/bin/Debug/net461/JsonFx.Json.dll ./JsonFx-Unity/Assets/Tests/JsonFx.Json.dll
-cp ./JsonFx/JsonFx.Json.UnitTests/bin/Debug/net461/JsonFx.Json.UnitTests.dll ./JsonFx-Unity/Assets/Tests/JsonFx.Json.UnitTests.dll
+echo "hello"
+#cp ./JsonFx/JsonFx.Json.UnitTests/bin/Debug/net461/JsonFx.Json.dll ./JsonFx-Unity/Assets/Tests/JsonFx.Json.dll
+#cp ./JsonFx/JsonFx.Json.UnitTests/bin/Debug/net461/JsonFx.Json.UnitTests.dll ./JsonFx-Unity/Assets/Tests/JsonFx.Json.UnitTests.dll
