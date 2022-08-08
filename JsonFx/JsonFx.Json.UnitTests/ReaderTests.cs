269e74db6c1071188bc42d2c156d62b0bc3051ec
Ricky Willis <ricky.willis@spaceapegames.com>
2022-08-08 16:06:05 +0100

    Add a test for the InvalidCastException

---
+++ JsonFx/JsonFx.Json.UnitTests/ReaderTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace JsonFx.Json.Tests
+{
+    [TestFixture]
+    public class ReaderTests
+    {
+        [Test]
+        public void TestDeserializationWithMixedTypeArrays()
+        {
+            // This test checks a case discovered when migrating to Unity 2021.
+            // This threw an InvalidCastException in Unity 2021 but not when testing in 2019.
+            // It also did not throw when running the tests directly in this sln.
+            var deserialized = JsonReader.Deserialize("[[\"id\",\"name\"],[1,\"myname\"]]");
+            Assert.IsNotNull(deserialized);
+        }
+    }
+}
