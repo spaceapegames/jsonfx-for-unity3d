76147a1f618f4414fde1b62ad65fbf54a94e23d2
Ricky Willis <ricky.willis@spaceapegames.com>
2022-08-08 18:24:39 +0100

    Revert "test script change to see if the build permission error is due to running the script or the script contents"
    This reverts commit bd024c2a804a3facb65c7aee071eb818a1522699.

--- copy-dlls-to-unity.sh
+++ copy-dlls-to-unity.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "hello"
-#cp ./JsonFx/JsonFx.Json.UnitTests/bin/Debug/net461/JsonFx.Json.dll ./JsonFx-Unity/Assets/Tests/JsonFx.Json.dll
-#cp ./JsonFx/JsonFx.Json.UnitTests/bin/Debug/net461/JsonFx.Json.UnitTests.dll ./JsonFx-Unity/Assets/Tests/JsonFx.Json.UnitTests.dll
+cp ./JsonFx/JsonFx.Json.UnitTests/bin/Debug/net461/JsonFx.Json.dll ./JsonFx-Unity/Assets/Tests/JsonFx.Json.dll
+cp ./JsonFx/JsonFx.Json.UnitTests/bin/Debug/net461/JsonFx.Json.UnitTests.dll ./JsonFx-Unity/Assets/Tests/JsonFx.Json.UnitTests.dll
