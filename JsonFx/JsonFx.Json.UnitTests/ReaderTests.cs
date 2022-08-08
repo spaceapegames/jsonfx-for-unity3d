0cdeab3bc03f3055cd99b49e3d4f022f157b87b9
Ricky Willis <ricky.willis@spaceapegames.com>
2022-08-08 18:19:46 +0100

    add some more tests to check other types of arrays-of-arrays

--- JsonFx/JsonFx.Json.UnitTests/ReaderTests.cs
+++ JsonFx/JsonFx.Json.UnitTests/ReaderTests.cs
@@ -12,9 +12,28 @@ namespace JsonFx.Json.Tests
             // This threw an InvalidCastException in Unity 2021 but not when testing in 2019.
             // It also did not throw when running the tests directly in this sln.
             var deserialized = JsonReader.Deserialize("[[\"id\",\"name\"],[1,\"myname\"]]");
-
             Assert.IsNotNull(deserialized);
             Assert.IsInstanceOf<object[][]>(deserialized);
+        }
+
+        [Test]
+        public void TestDeserializationWithHomogeneousArrays()
+        {
+            string strJson = "[[\"id\",\"name\"],[\"1\",\"myname\"]]";
+            TestDeserialization<string[][]>(strJson);
+
+            string intJson = "[[1,2],[3,4]]";
+            TestDeserialization<int[][]>(intJson);
+
+            string doubleJson = "[[1.2,2.3],[3.5,4.3]]";
+            TestDeserialization<double[][]>(doubleJson);
+        }
+
+        private void TestDeserialization<T>(string json)
+        {
+            var deserialized = JsonReader.Deserialize(json);
+            Assert.IsNotNull(deserialized);
+            Assert.IsInstanceOf<T>(deserialized);
         }
     }
 }
