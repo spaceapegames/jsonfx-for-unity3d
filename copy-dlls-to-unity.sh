86a58a966570bb0b1b87c198d02522c2625a4b86
Ricky Willis <ricky.willis@spaceapegames.com>
2022-08-09 11:58:30 +0100

    Improve dll copy script from PR feedback

--- copy-dlls-to-unity.sh
+++ copy-dlls-to-unity.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-set -e
-set -x
-
+cd "$(dirname "$0")"
 cp ./JsonFx/JsonFx.Json.UnitTests/bin/Release/net461/JsonFx.Json.dll ./JsonFx-Unity/Assets/Tests/Editor/JsonFx.Json.dll
 cp ./JsonFx/JsonFx.Json.UnitTests/bin/Release/net461/JsonFx.Json.UnitTests.dll ./JsonFx-Unity/Assets/Tests/Editor/JsonFx.Json.UnitTests.dll
