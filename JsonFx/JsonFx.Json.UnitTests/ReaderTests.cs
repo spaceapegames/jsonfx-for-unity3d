955c6e6664b7801a83fb9c253b95cce671dab7a8
Ricky Willis <ricky.willis@spaceapegames.com>
2022-08-09 12:09:29 +0100

    spaces -> tabs, more test cases

--- JsonFx/JsonFx.Json.UnitTests/ReaderTests.cs
+++ JsonFx/JsonFx.Json.UnitTests/ReaderTests.cs
@@ -6,27 +6,49 @@ namespace JsonFx.Json.Tests
     public class ReaderTests
     {
         [Test]
-        public void TestDeserializationWithMixedTypeArrays()
+        [TestCase("[[\"id\",\"name\"],[1,\"myname\"]]")]
+        [TestCase("[[\"id\",\"name\"],[null]]")]
+        [TestCase("[[\"id\",\"name\"],[1, null]]")]
+        [TestCase("[[null,\"name\"],[{}, {}]]")]
+        [TestCase("[[null,\"name\"],[1, true]]")]
+        [TestCase("[[false,\"name\"],[1, null]]")]
+        [TestCase("[[{},{}],null]")]
+        public void TestDeserializationWithMixedTypeArrays(string json)
         {
             // This test checks a case discovered when migrating to Unity 2021.
             // This threw an InvalidCastException in Unity 2021 but not when testing in 2019.
             // It also did not throw when running the tests directly in this sln.
-            var deserialized = JsonReader.Deserialize("[[\"id\",\"name\"],[1,\"myname\"]]");
+            var deserialized = JsonReader.Deserialize(json);
             Assert.IsNotNull(deserialized);
             Assert.IsInstanceOf<object[][]>(deserialized);
         }
 
         [Test]
-        public void TestDeserializationWithHomogeneousArrays()
+        public void TestDeserializationWithStringArrays()
         {
-            string strJson = "[[\"id\",\"name\"],[\"1\",\"myname\"]]";
-            TestDeserialization<string[][]>(strJson);
+            string json = "[[\"id\",\"name\"],[\"1\",\"myname\"]]";
+            TestDeserialization<string[][]>(json);
+        }
 
-            string intJson = "[[1,2],[3,4]]";
-            TestDeserialization<int[][]>(intJson);
+        [Test]
+        public void TestDeserializationWithIntArrays()
+        {
+            string json = "[[1,2],[3,4]]";
+            TestDeserialization<int[][]>(json);
+        }
 
-            string doubleJson = "[[1.2,2.3],[3.5,4.3]]";
-            TestDeserialization<double[][]>(doubleJson);
+        [Test]
+        public void TestDeserializationWithDoubleArrays()
+        {
+            string json = "[[1.2,2.3],[3.5,4.3]]";
+            TestDeserialization<double[][]>(json);
+        }
+
+        [Test]
+        public void TestDeserializationWithBoolArrays()
+        {
+            string json = "[[true, false],[true, true]]";
+            TestDeserialization<bool[][]>(json);
         }
 
         private void TestDeserialization<T>(string json)
